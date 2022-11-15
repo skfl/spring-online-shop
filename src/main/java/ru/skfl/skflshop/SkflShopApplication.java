7412a93b76f496cfa654f84179d33dcd9c59ba1a
Edward Rogachkov <ctz3n4cs@gmail.com>
2022-11-15 12:46:50 +0400

    init commit

---
+++ src/main/java/ru/skfl/skflshop/SkflShopApplication.java
@@ -0,0 +1,13 @@
+package ru.skfl.skflshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkflShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkflShopApplication.class, args);
+	}
+
+}
